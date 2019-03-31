0dea4ae1e2db385577911ad26975cc6b6a025ed2
root <root@DellsonDaSilva.localdomain>
2019-03-31 17:37:49 -0300

    First Commit com tela inicial de login

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img class="Logo-PaggCerto" alt="Logo da PaggCerto" src="./assets/PaggCerto.svg" />
+    <Signin />
   </div>
 </template>
 
 <script>
-import Signin from './components/Signin.vue'
+import Signin from './components/Signin.vue';
 
 export default {
   name: 'app',
   components: {
-    Signin
+    Signin,
+
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+@import './styles/style.scss';
 </style>
